26abf7803dcd178b60fa30acc52392c76300e358
ChaitanyaSudha <chaitanyasudhab9@gmail.com>
2026-08-13 12:38:18 +0530

    My first commit message

---
+++ first/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static('front end'));
+
+app.listen(port, () => {
+  console.log("Server running on http://localhost:" + port);
+});
